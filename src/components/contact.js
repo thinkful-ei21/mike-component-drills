6aa5e77e08508a4cdb63684786d4c704cdad8a22
Michael Le-Reiver <m.lereiver@gmail.com>
2018-06-18 12:23:31 -0700

    completed adrress-book-challenge

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,14 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+
+export default function Contact(props) {
+  return (
+    <div id={props.index} className='contact'>
+      <img src={props.photo} className='contact-photo' alt='user-photo' />
+      <h3 className='contact-name'>{props.name}</h3>
+      <p className='contact-address'>{props.address}</p>
+    </div>
+  );
+
+}
